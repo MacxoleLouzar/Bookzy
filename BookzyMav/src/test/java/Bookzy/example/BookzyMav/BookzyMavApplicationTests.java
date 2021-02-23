add0ec4abc21c4cfdda86562190953136d2b2957
MacxoleLouzar <37369433+MacxoleLouzar@users.noreply.github.com>
2021-02-23 18:00:06 +0000

    Add files via upload

---
+++ BookzyMav/src/test/java/Bookzy/example/BookzyMav/BookzyMavApplicationTests.java
@@ -0,0 +1,13 @@
+package Bookzy.example.BookzyMav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyMavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
